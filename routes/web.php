25db66b839d3bf8a3d0d0b5ab3c7c833b3324df3
BeatrizJarbas-dev <biajarbasjensen@gmail.com>
2025-10-27 14:56:10 -0300

    verificação de email

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,11 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+Route::middleware(['auth' ,'verified'])->group(function () {
+    Route::get('/dashboard', function(){
+        return view ('dashboard');       
+})-> name('dashboard');
+});
 
 // Rotas de administrador
 Route::middleware(['auth', 'admin'])->group(function () {
