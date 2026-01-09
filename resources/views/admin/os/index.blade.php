60d3430bfe988dc7711a1dd163cbc91404d96f10
Akrvx <mauriciolc.codes@gmail.com>
2026-01-09 16:31:45 -0300

    N Protocolo tela ADM

--- resources/views/admin/os/index.blade.php
+++ resources/views/admin/os/index.blade.php
@@ -41,7 +41,7 @@
             <table class="min-w-full divide-y divide-gray-200">
                 <thead class="bg-gray-50">
                     <tr>
-                        <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">#OS</th>
+                        <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Nº Protocolo</th>
                         
                         {{-- CABEÇALHO 1: RESPONSÁVEL --}}
                         <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
