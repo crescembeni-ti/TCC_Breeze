e774a8df66a525ed68cd696c732dc01302c9f4fb
juanferreira-x <197127883+juanferreira-x@users.noreply.github.com>
2026-02-03 09:35:19 -0500

    Fix: Corrigir redirecionamento incorreto no login do analista

--- app/Http/Controllers/Analista/AuthenticatedSessionController.php
+++ app/Http/Controllers/Analista/AuthenticatedSessionController.php
@@ -34,7 +34,7 @@ class AuthenticatedSessionController extends Controller
 
             $request->session()->regenerate();
 
-            return redirect()->intended(route('analyst.dashboard'));
+            return redirect()->route('analyst.dashboard');
         }
 
         throw ValidationException::withMessages([
